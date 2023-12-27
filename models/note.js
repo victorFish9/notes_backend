7e67fa0b913c2835c2692f24c68d5656fd013053
Victor <aue228lublupacanov@gmail.com>
2023-12-27 15:23:04 +0200

    created boudn of tests

--- models/note.js
+++ models/note.js
@@ -1,19 +1,5 @@
 const mongoose = require('mongoose')
 
-//const password = process.argv[2]
-mongoose.set('strictQuery', false)
-
-//`mongodb+srv://victorcherkasov222:${password}@cluster0.kkdi9xk.mongodb.net/noteApp?retryWrites=true&w=majority`
-const url = process.env.MONGODB_URI
-
-console.log('connecting to ', url)
-
-mongoose.connect(url).then(result => {
-  console.log('connected to Mongodb')
-})
-  .catch((error) => {
-    console.log('error connecting to MongoDB', error.message)
-  })
 
 const noteSchema = new mongoose.Schema({
   content: {
