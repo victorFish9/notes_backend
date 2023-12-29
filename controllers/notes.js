c3c7628f5b38c3f4a2b219f26c21b83c5d510e1d
Victor <aue228lublupacanov@gmail.com>
2023-12-29 17:03:59 +0200

    token system for login created

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,5 +1,16 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
 
 notesRouter.get('/', async (request, response) => {
     const notes = await Note.find({})
@@ -15,15 +26,27 @@ notesRouter.get('/:id', async (request, response, next) => {
     }
 })
 
-notesRouter.post('/', async (request, response, next) => {
+
+
+notesRouter.post('/', async (request, response) => {
     const body = request.body
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
 
     const note = new Note({
         content: body.content,
-        important: body.important || false
+        important: body.important === undefined ? false : body.important,
+        user: user._id
     })
     const savedNote = await note.save()
-    response.status(201).json(savedNote)
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
+
+    response.json(savedNote)
 })
 
 notesRouter.delete('/:id', async (request, response) => {
--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose')
 const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
+const bcrypt = require('bcrypt')
+
 
 const Note = require('../models/note')
+const User = require('../models/user')
 
 describe('when there is initially some notes saved', () => {
     beforeEach(async () => {
@@ -32,6 +35,58 @@ describe('when there is initially some notes saved', () => {
         expect(contents).toContain(
             'Browser can execute only JavaScript'
         )
+    })
+
+    describe('when there is initially one user at db', () => {
+        beforeEach(async () => {
+            await User.deleteMany({})
+
+            const passwordHash = await bcrypt.hash('sekret', 10)
+            const user = new User({ username: 'root', passwordHash })
+
+            await user.save()
+        })
+        test('creation succeeds with a fresh username', async () => {
+            const usersAtStart = await helper.usersInDb()
+
+            const newUser = {
+                username: 'mluukkai',
+                name: 'Matti Luukkainen',
+                password: 'salainen',
+            }
+
+            await api.post('api/users')
+                .send(newUser)
+                .expect(201)
+                .expect('Content-Type', /application\/json/)
+
+            const usersAtEnd = await helper.usersInDb()
+            expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+            const usernames = usersAtEnd.map(u => u.username)
+            expect(usernames).toContain(newUser.username)
+        })
+        test('creation fails with proper statuscode and message if username already taken',
+            async () => {
+                const usersAtStart = await helper.usersInDb()
+
+                const newUser = {
+                    username: 'root',
+                    name: 'Sueruser',
+                    password: 'salainen'
+                }
+
+                const result = await api
+                    .post('/api/users')
+                    .send(newUser)
+                    .expect(400)
+                    .expect('Content-Type', /application\/json/)
+
+                expect(result.body.error).toContain('expected `username` to be unique')
+
+                const usersAtEnd = await helper.usersInDb()
+                expect(usersAtEnd).toHaveLength(usersAtStart.length)
+            })
     })
 
     describe('viewing a specific note', () => {
